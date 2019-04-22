9a6f66664ac7586dcac2b13add95e4eec3903d9a
Serena Toon <stoo718@aucklanduni.ac.nz>
2019-04-22 22:28:41 +1200

    get repos

--- analytics.py
+++ analytics.py
@@ -1,12 +1,25 @@
 import requests
 import sys
+import json
+
+GITHUB_ENDPOINT = "https://api.github.com/"
+
+def get_repos(username):
+    global GITHUB_ENDPOINT
+    endpoint = GITHUB_ENDPOINT + "users/" + username + "/repos"
+    print endpoint
+    req = requests.get(endpoint)
+    json_data = req.json()
+    for key in json_data:
+        print key['full_name']
 
 
 def main(username, password):
-    endpoint = "https://api.github.com/repos/serenatoon/spacelords/traffic/views"
+    get_repos(username)
+    # endpoint = "https://api.github.com/repos/serenatoon/spacelords/traffic/views"
 
-    req = requests.get(endpoint, auth=(username, password))
-    print req.json()
+    # req = requests.get(endpoint, auth=(username, password))
+    # print req.json()
 
 
 if __name__ == "__main__":
