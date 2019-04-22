6bab1bd86ce278358ee180628631dcc4c4f06689
Serena Toon <stoo718@aucklanduni.ac.nz>
2019-04-22 22:06:45 +1200

    take username and pw as params to script

--- analytics.py
+++ analytics.py
@@ -1,7 +1,16 @@
 import requests
+import sys
 
-endpoint = "https://api.github.com/repos/serenatoon/spacelords/traffic/views"
-auth = "serenatoon"
 
-req = requests.get(endpoint, auth=('serenatoon', ''))
-print req.json()
+def main(username, password):
+    endpoint = "https://api.github.com/repos/serenatoon/spacelords/traffic/views"
+
+    req = requests.get(endpoint, auth=(username, password))
+    print req.json()
+
+
+if __name__ == "__main__":
+    if len(sys.argv) < 3:
+        print "Script usage: analytics.py USERNAME PASSWORD"
+    else:
+        main(sys.argv[1], sys.argv[2])
